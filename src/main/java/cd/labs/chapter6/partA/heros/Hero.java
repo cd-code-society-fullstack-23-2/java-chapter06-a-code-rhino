806480c3dfa72c521cd1fcc4de193d5938de65c1
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2023-08-31 14:54:41 -0400

    Did it for the people

--- src/main/java/cd/labs/chapter6/partA/heros/Hero.java
+++ src/main/java/cd/labs/chapter6/partA/heros/Hero.java
@@ -1,4 +1,57 @@
 package cd.labs.chapter6.partA.heros;
 
 public class Hero {
+    private static int heroCount;
+
+    private String name;
+    private String alias;
+    private String [] superPowers;
+    private String affiliation;
+    private Integer healthPoints;
+
+    public Hero(String name,
+                String alias,
+                String affiliation){
+        this.name = name;
+        this.alias = alias;
+        this.affiliation = affiliation;
+        superPowers = new String[5];
+        healthPoints = 0;
+        heroCount++;
+    }
+
+    public Boolean addSuperPower(String power){
+        for(int x = 0; x < superPowers.length; x++){
+            if(superPowers[x] == null){
+                superPowers[x] = power;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String[] displaySuperPowers(){
+        return superPowers;
+    }
+
+    public Boolean removePower(String power){
+        for(int x = 0; x < superPowers.length; x++){
+            String p = superPowers[x];
+            if(p != null && p.equals(power)){
+                superPowers[x] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String teamUp(Hero hero){
+        String msg = String.format("%s teams up with %s!",this.alias,hero.alias);
+        return msg;
+    }
+
+    public static int getHeroCount(){
+        return heroCount;
+    }
+
 }
--- src/test/java/cd/labs/chapter6/partA/heros/HeroTest.java
+++ src/test/java/cd/labs/chapter6/partA/heros/HeroTest.java
@@ -1,4 +1,86 @@
 package cd.labs.chapter6.partA.heros;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class HeroTest {
+
+    @Test
+    public void addAndDisplayTest01(){
+        // Given
+        Hero hero = new Hero("Luke Cage","Powerman", "Defenders");
+        String power = "Super Strength";
+
+        // Then
+        hero.addSuperPower(power);
+        String[] expectedPowers = new String[5];
+        expectedPowers[0] = power;
+        String[] actualPowers = hero.displaySuperPowers();
+        // When
+        assertArrayEquals(expectedPowers, actualPowers);
+    }
+
+    @Test
+    public void addSuperPowerTest01(){
+        // Given
+        Hero hero = new Hero("Luke Cage","Powerman", "Defenders");
+        hero.addSuperPower("Super Strength");
+        hero.addSuperPower("Durability");
+        hero.addSuperPower("Healing Factor");
+        hero.addSuperPower("Power of Christmas");
+        hero.addSuperPower("Black man");
+
+        // expected
+        Boolean actual = hero.addSuperPower("Atomic Dawg");
+
+        assertFalse(actual);
+    }
+
+    @Test
+    public void removePowerTest01(){
+        // Given
+        Hero hero = new Hero("Jason Todd","Red Hood", "Bat Family");
+        hero.addSuperPower("Resurrection");
+
+        // When
+        Boolean actual = hero.removePower("Resurrection");
+
+        // Then
+        assertTrue(actual);
+    }
+
+    @Test
+    public void removePowerTest02(){
+        // Given
+        Hero hero = new Hero("Jason Todd","Red Hood", "Bat Family");
+        hero.addSuperPower("Resurrection");
+
+        // When
+        Boolean actual = hero.removePower("Super Strength");
+
+        // Then
+        assertFalse(actual);
+    }
+
+    @Test
+    public void teamUpTest01(){
+        Hero frank = new Hero("Frank Castle","The Punisher", "His Damn self");
+        Hero luke = new Hero("Luke Cage","Powerman", "Defenders");
+
+        String expectMsg = "Powerman teams up with The Punisher!";
+        String actualMsg = luke.teamUp(frank);
+
+        assertEquals(expectMsg, actualMsg);
+    }
+
+    @Test
+    public void getHeroCountTest01(){
+        Hero frank = new Hero("Frank Castle","The Punisher", "His Damn self");
+
+        int expected = 1;
+        int actual = Hero.getHeroCount();
+
+        assertEquals(expected, actual);
+    }
+
 }
