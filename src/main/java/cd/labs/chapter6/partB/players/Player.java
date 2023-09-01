3848a81b9fbbd61fa838f003bbd994334e7220f8
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2023-09-01 11:55:24 -0400

    updates

--- src/main/java/cd/labs/chapter6/partB/players/Player.java
+++ src/main/java/cd/labs/chapter6/partB/players/Player.java
@@ -1,4 +1,45 @@
 package cd.labs.chapter6.partB.players;
 
+import java.util.ArrayList;
+
 public class Player {
+    private static int playerCount;
+    private String name;
+    private Integer age;
+    private ArrayList<String> skills;
+    private String position;
+    private Integer goalsScored;
+
+    public Player(String name, int age, String position){
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        skills = new ArrayList<>();
+        goalsScored = 0;
+        playerCount++;
+    }
+
+    public Boolean addSkill(String skill){
+        if(skills.size() >=5)
+            return false;
+        skills.add(skill);
+        return true;
+    }
+
+    public Boolean removeSkill(String skill){
+        return skills.remove(skill);
+    }
+
+    public ArrayList<String> displaySkills(){
+        return skills;
+    }
+
+    public String teamUpWith(Player player){
+        String response = String.format("%s has teamed up with %s",this.name, player.name);
+        return response;
+    }
+
+    public static int getPlayerCount(){
+        return playerCount;
+    }
 }
--- src/test/java/cd/labs/chapter6/partB/players/PlayerTest.java
+++ src/test/java/cd/labs/chapter6/partB/players/PlayerTest.java
@@ -1,4 +1,77 @@
 package cd.labs.chapter6.partB.players;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 public class PlayerTest {
+
+    @Test
+    public void addDisplaySkillsTest01(){
+        // Given
+        Player player = new Player("Hamlet", 23  , "Pitcher");
+        String skillToAdd = "Curve Ball";
+
+        // When
+        ArrayList<String> expectedSkills = new ArrayList<>();
+        expectedSkills.add(skillToAdd);
+
+        player.addSkill(skillToAdd);
+        ArrayList<String> actualSkills = player.displaySkills();
+        // Then
+        Assertions.assertEquals(expectedSkills.size(), actualSkills.size());
+        Assertions.assertEquals(expectedSkills.get(0), actualSkills.get(0));
+    }
+
+    @Test
+    public void addMoreThan5SkillsTest01(){
+        Player player = new Player("Hamlet", 23  , "Pitcher");
+        String skillToAdd = "Curve Ball";
+        for (int x = 0; x < 5; x++){
+            player.addSkill(skillToAdd);
+        }
+        Boolean actual = player.addSkill(skillToAdd);
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void removeSkillTest01(){
+        Player player = new Player("Hamlet", 23  , "Pitcher");
+        String skillToAdd = "Curve Ball";
+        player.addSkill(skillToAdd);
+        Boolean actual = player.removeSkill(skillToAdd);
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void removeSkillTest02(){
+        Player player = new Player("Hamlet", 23  , "Pitcher");
+        String skillToAdd = "Curve Ball";
+        Boolean actual = player.removeSkill(skillToAdd);
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void teamUpWithTest01(){
+        Player player1 = new Player("Mario", 23  , "Pitcher");
+        Player player2 = new Player("Lugi", 23  , "Catcher");
+
+        String expected = "Mario has teamed up with Lugi";
+        String actual = player1.teamUpWith(player2);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getPlayerCount(){
+        Player player1 = new Player("Mario", 23  , "Pitcher");
+        Player player2 = new Player("Lugi", 23  , "Catcher");
+
+        int expected = 2;
+        int actual = Player.getPlayerCount();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
